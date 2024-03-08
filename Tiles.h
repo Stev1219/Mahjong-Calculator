c57f74d70194a09b36ad78e0023d7d9ae5b77431
Steve Laptop <stevensejasc@gmail.com>
2024-03-08 18:07:53 -0500

    Added up to Hand14

--- Tiles.h
+++ Tiles.h
@@ -6,68 +6,80 @@ enum class Wind
     East, South, West, North, None
 };
 
+enum class Dragon 
+{
+    Red, White, Green, None
+};
+
+
 enum class Color
 {
-    Circle,Bamboo,Character, None
+    Circle,Bamboo,Character,None
 };
 
 class Tile 
 {
 public:
-    Tile(Color color, int number, Wind wind)
+    Tile(Color color, int number, Wind wind, Dragon dragon)
     {
         this->color = color;
         this->number = number;
         this->wind= wind;
+        this->dragon= dragon;
     }
     Color color;
     int number;
     Wind wind;
+    Dragon dragon;
 };
 
-const Tile Bamboo1 (Color::Bamboo,1, Wind::None);
-const Tile Bamboo2 (Color::Bamboo,2, Wind::None);
-const Tile Bamboo3 (Color::Bamboo,3, Wind::None);
-const Tile Bamboo4 (Color::Bamboo,4, Wind::None);
-const Tile Bamboo5 (Color::Bamboo,5, Wind::None);
-const Tile Bamboo6 (Color::Bamboo,6, Wind::None);
-const Tile Bamboo7 (Color::Bamboo,7, Wind::None);
-const Tile Bamboo8 (Color::Bamboo,8, Wind::None);
-const Tile Bamboo9 (Color::Bamboo,9, Wind::None);
+const Tile Bamboo1 (Color::Bamboo,1, Wind::None, Dragon::None);
+const Tile Bamboo2 (Color::Bamboo,2, Wind::None, Dragon::None);
+const Tile Bamboo3 (Color::Bamboo,3, Wind::None, Dragon::None);
+const Tile Bamboo4 (Color::Bamboo,4, Wind::None, Dragon::None);
+const Tile Bamboo5 (Color::Bamboo,5, Wind::None, Dragon::None);
+const Tile Bamboo6 (Color::Bamboo,6, Wind::None, Dragon::None);
+const Tile Bamboo7 (Color::Bamboo,7, Wind::None, Dragon::None);
+const Tile Bamboo8 (Color::Bamboo,8, Wind::None, Dragon::None);
+const Tile Bamboo9 (Color::Bamboo,9, Wind::None, Dragon::None);
 
-const Tile Circle1 (Color::Circle,1, Wind::None);
-const Tile Circle2 (Color::Circle,2, Wind::None);
-const Tile Circle3 (Color::Circle,3, Wind::None);
-const Tile Circle4 (Color::Circle,4, Wind::None);
-const Tile Circle5 (Color::Circle,5, Wind::None);
-const Tile Circle6 (Color::Circle,6, Wind::None);
-const Tile Circle7 (Color::Circle,7, Wind::None);
-const Tile Circle8 (Color::Circle,8, Wind::None);
-const Tile Circle9 (Color::Circle,9, Wind::None);
+const Tile Circle1 (Color::Circle,1, Wind::None, Dragon::None);
+const Tile Circle2 (Color::Circle,2, Wind::None, Dragon::None);
+const Tile Circle3 (Color::Circle,3, Wind::None, Dragon::None);
+const Tile Circle4 (Color::Circle,4, Wind::None, Dragon::None);
+const Tile Circle5 (Color::Circle,5, Wind::None, Dragon::None);
+const Tile Circle6 (Color::Circle,6, Wind::None, Dragon::None);
+const Tile Circle7 (Color::Circle,7, Wind::None, Dragon::None);
+const Tile Circle8 (Color::Circle,8, Wind::None, Dragon::None);
+const Tile Circle9 (Color::Circle,9, Wind::None, Dragon::None);
 
-const Tile Character1 (Color::Character,1, Wind::None);
-const Tile Character2 (Color::Character,2, Wind::None);
-const Tile Character3 (Color::Character,3, Wind::None);
-const Tile Character4 (Color::Character,4, Wind::None);
-const Tile Character5 (Color::Character,5, Wind::None);
-const Tile Character6 (Color::Character,6, Wind::None);
-const Tile Character7 (Color::Character,7, Wind::None);
-const Tile Character8 (Color::Character,8, Wind::None);
-const Tile Character9 (Color::Character,9, Wind::None);
+const Tile Character1 (Color::Character,1, Wind::None, Dragon::None);
+const Tile Character2 (Color::Character,2, Wind::None, Dragon::None);
+const Tile Character3 (Color::Character,3, Wind::None, Dragon::None);
+const Tile Character4 (Color::Character,4, Wind::None, Dragon::None);
+const Tile Character5 (Color::Character,5, Wind::None, Dragon::None);
+const Tile Character6 (Color::Character,6, Wind::None, Dragon::None);
+const Tile Character7 (Color::Character,7, Wind::None, Dragon::None);
+const Tile Character8 (Color::Character,8, Wind::None, Dragon::None);
+const Tile Character9 (Color::Character,9, Wind::None, Dragon::None);
 
-const Tile WindEast (Color::None,0, Wind::East);
-const Tile WindSouth (Color::None,0, Wind::South);
-const Tile WindWest (Color::None,0, Wind::West);
-const Tile WindNorth (Color::None,0, Wind::North);
+const Tile WindEast (Color::None,0, Wind::East, Dragon::None);
+const Tile WindSouth (Color::None,0, Wind::South, Dragon::None);
+const Tile WindWest (Color::None,0, Wind::West, Dragon::None);
+const Tile WindNorth (Color::None,0, Wind::North, Dragon::None);
 
-const Tile Flower1 (Color::None,1, Wind::None);
-const Tile Flower2 (Color::None,2, Wind::None);
-const Tile Flower3 (Color::None,3, Wind::None);
-const Tile Flower4 (Color::None,4, Wind::None);
+const Tile DragonRed (Color::None,0, Wind::None, Dragon::Red);
+const Tile DragonWhite (Color::None,0, Wind::None, Dragon::White);
+const Tile DragonGreen (Color::None,0, Wind::None, Dragon::Green);
 
-const Tile Season1 (Color::None,1, Wind::None);
-const Tile Season2 (Color::None,2, Wind::None);
-const Tile Season3 (Color::None,3, Wind::None);
-const Tile Season4 (Color::None,4, Wind::None);
+const Tile Flower1 (Color::None,1, Wind::None, Dragon::None);
+const Tile Flower2 (Color::None,2, Wind::None, Dragon::None);
+const Tile Flower3 (Color::None,3, Wind::None, Dragon::None);
+const Tile Flower4 (Color::None,4, Wind::None, Dragon::None);
+
+const Tile Season1 (Color::None,1, Wind::None, Dragon::None);
+const Tile Season2 (Color::None,2, Wind::None, Dragon::None);
+const Tile Season3 (Color::None,3, Wind::None, Dragon::None);
+const Tile Season4 (Color::None,4, Wind::None, Dragon::None);
 
 }
--- testing.cpp
+++ testing.cpp
@@ -45,25 +45,36 @@ bool IsPung(std::vector<Tile> tiles, Color coloryes)
 
     if (n==3)
     {
-    int i=0;
-    bool j=true;
-    bool k=true;
-    for (i=0; i<3; i++)
-    {
-        j=tiles[i].color==coloryes;
-    }
-    if (j==true)
-    {
-        for (i=0; i<2; i++)
+        int i=0;
+        bool j;
+        bool k;
+        bool l;
+        for (i=0; i<3; i++)
         {
-            k=(tiles[i].number)==tiles[i+1].number;
+            j=tiles[i].color==coloryes;
         }
-        return k;
-    }
-    else
-    {
-        return j;
-    }
+        if (j==true)
+        {
+            k=(tiles[0].number==tiles[1].number)&&(tiles[0].number==tiles[2].number);
+            
+            if (tiles[0].dragon!=Dragon::None)
+            {
+                l=(tiles[0].dragon==tiles[1].dragon)&&(tiles[0].dragon==tiles[2].dragon);
+            }
+            else if (tiles[0].wind!=Wind::None)
+            {
+                l=(tiles[0].wind==tiles[1].wind)&&(tiles[0].wind==tiles[2].wind);
+            }
+            else
+            {
+                return k;
+            }
+            return k&&l;
+        }
+        else
+        {
+            return j;
+        }
     }
     else {return false;}
 }
@@ -75,25 +86,37 @@ bool IsKong(std::vector<Tile> tiles, Color coloryes)
 
     if (n==4)
     {
-    int i=0;
-    bool j=true;
-    bool k=true;
-    for (i=0; i<4; i++)
-    {
-        j=tiles[i].color==coloryes;
-    }
-    if (j==true)
-    {
-        for (i=0; i<3; i++)
+        int i=0;
+        bool j;
+        bool k;
+        bool l;
+        for (i=0; i<4; i++)
         {
-            k=(tiles[i].number)==tiles[i+1].number;
+            j=tiles[i].color==coloryes;
         }
-        return k;
-    }
-    else
-    {
-        return j;
-    }
+        if (j==true)
+        {
+            k=(tiles[0].number==tiles[1].number)&&(tiles[0].number==tiles[2].number)&&(tiles[0].number==tiles[3].number);
+
+            if (tiles[0].dragon!=Dragon::None)
+            {
+                l=(tiles[0].dragon==tiles[1].dragon)&&(tiles[0].dragon==tiles[2].dragon)&&(tiles[0].dragon==tiles[3].dragon);
+                return k&&l;
+            }
+            else if (tiles[0].wind!=Wind::None)
+            {
+                l=(tiles[0].wind==tiles[1].wind)&&(tiles[0].wind==tiles[2].wind)&&(tiles[0].wind==tiles[3].wind);
+                return k&&l;
+            }
+            else
+            {
+                return k;
+            }
+        }
+        else
+        {
+            return j;
+        }
     }
     else {return false;}
 }
@@ -178,20 +201,44 @@ bool Hand05 (std::vector <Tile> set1, std::vector <Tile> set2, std::vector <Tile
     bool a=IsPung(set1, set1[0].color)&&(set1[0].number==1);
     bool b=IsPung(set1, set1[0].color)&&(set1[0].number==9);
     bool c=IsPung(set1, set1[0].color)&&(set1[0].wind!=Wind::None)&&(set1[0].wind!=seat)&&(set1[0].wind!=prevail);
+    bool aa=IsPung(set1, set1[0].color)&&(set1[0].dragon!=Dragon::None);
+
+    bool a1=IsKong(set1, set1[0].color)&&(set1[0].number==1);
+    bool b1=IsKong(set1, set1[0].color)&&(set1[0].number==9);
+    bool c1=IsKong(set1, set1[0].color)&&(set1[0].wind!=Wind::None)&&(set1[0].wind!=seat)&&(set1[0].wind!=prevail);
+    bool aa1=IsKong(set1, set1[0].color)&&(set1[0].dragon!=Dragon::None);
 
     bool d=IsPung(set2, set2[0].color)&&(set2[0].number==1);
     bool e=IsPung(set2, set2[0].color)&&(set2[0].number==9);
     bool f=IsPung(set2, set2[0].color)&&(set2[0].wind!=Wind::None)&&(set2[0].wind!=seat)&&(set2[0].wind!=prevail);
+    bool bb=IsPung(set2, set2[0].color)&&(set2[0].dragon!=Dragon::None);
+
+    bool d1=IsKong(set2, set2[0].color)&&(set2[0].number==1);
+    bool e1=IsKong(set2, set2[0].color)&&(set2[0].number==9);
+    bool f1=IsKong(set2, set2[0].color)&&(set2[0].wind!=Wind::None)&&(set2[0].wind!=seat)&&(set2[0].wind!=prevail);
+    bool bb1=IsKong(set2, set2[0].color)&&(set2[0].dragon!=Dragon::None);
 
     bool g=IsPung(set3, set3[0].color)&&(set3[0].number==1);
     bool h=IsPung(set3, set3[0].color)&&(set3[0].number==9);
     bool i=IsPung(set3, set3[0].color)&&(set3[0].wind!=Wind::None)&&(set3[0].wind!=seat)&&(set3[0].wind!=prevail);
+    bool cc=IsPung(set3, set3[0].color)&&(set3[0].dragon!=Dragon::None);
+
+    bool g1=IsKong(set3, set3[0].color)&&(set3[0].number==1);
+    bool h1=IsKong(set3, set3[0].color)&&(set3[0].number==9);
+    bool i1=IsKong(set3, set3[0].color)&&(set3[0].wind!=Wind::None)&&(set3[0].wind!=seat)&&(set3[0].wind!=prevail);
+    bool cc1=IsKong(set3, set3[0].color)&&(set3[0].dragon!=Dragon::None);
 
     bool j=IsPung(set4, set4[0].color)&&(set4[0].number==1);
     bool k=IsPung(set4, set4[0].color)&&(set4[0].number==9);
     bool l=IsPung(set4, set4[0].color)&&(set4[0].wind!=Wind::None)&&(set4[0].wind!=seat)&&(set4[0].wind!=prevail);
+    bool dd=IsPung(set4, set4[0].color)&&(set4[0].dragon!=Dragon::None);
 
-    bool total=a||b||c||d||e||f||g||h||i||j||k||l;
+    bool j1=IsKong(set4, set4[0].color)&&(set4[0].number==1);
+    bool k1=IsKong(set4, set4[0].color)&&(set4[0].number==9);
+    bool l1=IsKong(set4, set4[0].color)&&(set4[0].wind!=Wind::None)&&(set4[0].wind!=seat)&&(set4[0].wind!=prevail);
+    bool dd1=IsKong(set4, set4[0].color)&&(set4[0].dragon!=Dragon::None);
+
+    bool total=a||b||c||d||e||f||g||h||i||j||k||l||aa||bb||cc||dd||a1||b1||c1||d1||e1||f1||g1||h1||i1||j1||k1||l1||aa1||bb1||cc1||dd1;
 
     return total;
 
@@ -208,10 +255,36 @@ bool Hand06 (std::vector <Tile> set1, std::vector <Tile> set2, std::vector <Tile
     return total;
 }
 
-bool Hand07()
+bool Hand07(std::vector <Tile> set1, std::vector <Tile> set2, std::vector <Tile> set3, std::vector <Tile> set4, std::vector <Tile> pair)
 {
-    //3-INPUT NAND GATE FOR COLOR/SUITES, FIND A WAY TO GET RID OF THE 0 0 0 = TRUE STATEMENT
-    return false;
+    bool a=(set1[0].color!=Color::Character)&&(set2[0].color!=Color::Character)&&(set3[0].color!=Color::Character)&&(set4[0].color!=Color::Character)&&(pair[0].color!=Color::Character);
+    bool b=(set1[0].color!=Color::Bamboo)&&(set2[0].color!=Color::Bamboo)&&(set3[0].color!=Color::Bamboo)&&(set4[0].color!=Color::Bamboo)&&(pair[0].color!=Color::Bamboo);
+    bool c=(set1[0].color!=Color::Circle)&&(set2[0].color!=Color::Circle)&&(set3[0].color!=Color::Circle)&&(set4[0].color!=Color::Circle)&&(pair[0].color!=Color::Circle);
+    
+    bool total=a||b||c;
+    return total;
+}
+
+bool Hand08(std::vector <Tile> set1, std::vector <Tile> set2, std::vector <Tile> set3, std::vector <Tile> set4, std::vector <Tile> pair)
+{
+    bool a=(set1[0].color!=Color::None)&&(set2[0].color!=Color::None)&&(set3[0].color!=Color::None)&&(set4[0].color!=Color::None)&&(pair[0].color!=Color::None);
+    return a;
+}
+
+bool Hand14(std::vector <Tile> set1, std::vector <Tile> set2, std::vector <Tile> set3, std::vector <Tile> set4)
+{
+    bool a= IsPung(set1, set1[0].color)&&(set1[0].dragon!=Dragon::None);
+    bool b= IsPung(set2, set2[0].color)&&(set2[0].dragon!=Dragon::None);
+    bool c= IsPung(set3, set3[0].color)&&(set3[0].dragon!=Dragon::None);
+    bool d= IsPung(set4, set4[0].color)&&(set4[0].dragon!=Dragon::None);
+    bool e= IsKong(set1, set1[0].color)&&(set1[0].dragon!=Dragon::None);
+    bool f= IsKong(set2, set2[0].color)&&(set2[0].dragon!=Dragon::None);
+    bool g= IsKong(set3, set3[0].color)&&(set3[0].dragon!=Dragon::None);
+    bool h= IsKong(set4, set4[0].color)&&(set4[0].dragon!=Dragon::None);    
+    
+    bool total=a||b||c||d||e||f||g||h;
+
+    return total;
 }
 
 int main ()
@@ -219,20 +292,23 @@ int main ()
     Wind seat;
     Wind prevail;
     prevail=Wind::West;
-    seat=Wind::East;
+    seat=Wind::North;
     
-    Tile set1t1 (Bamboo1), set1t2 (Bamboo2), set1t3 (Bamboo3), set1t4 (Bamboo4);
+    Tile set1t1 (Bamboo4), set1t2 (Bamboo5), set1t3 (Bamboo6), set1t4 (Bamboo7);
     std::vector<Tile> vec1 = {set1t1, set1t2, set1t3};
 
     Tile set2t1 (Character1), set2t2 (Character1), set2t3 (Character1), set2t4 (Character1);
     std::vector<Tile> vec2 = {set2t1, set2t2, set2t3, set2t4};
 
-    Tile set3t1 (Character1), set3t2 (Character2), set3t3 (Character3), set3t4 (Character2);
+    Tile set3t1 (Character2), set3t2 (Character3), set3t3 (Character4), set3t4 (Character5);
     std::vector<Tile> vec3 = {set3t1, set3t2, set3t3};
     
-    Tile set4t1 (WindEast), set4t2 (WindEast), set4t3 (WindEast), set4t4 (Character5);
-    std::vector<Tile> vec4 = {set4t1, set4t2, set4t3};
+    Tile set4t1 (Bamboo1), set4t2 (Bamboo1), set4t3 (Bamboo1), set4t4 (Bamboo1);
+    std::vector<Tile> vec4 = {set4t1, set4t2, set4t3, set4t4};
     
+    Tile pair_t1 (Circle1), pair_t2 (Circle1);
+    std::vector<Tile> pair = {pair_t1, pair_t2};
+
     bool h01=Hand01(vec1, vec2, vec3, vec4);
     std::cout<<"Hand01: Pure Double Chow?"<<h01<<"\n";
 
@@ -250,6 +326,18 @@ int main ()
 
     bool h06=Hand06(vec1, vec2, vec3, vec4);
     std::cout<<"Hand06: Melded Kong?"<<h06<<"\n";
+
+    bool h07=Hand07(vec1, vec2, vec3, vec4, pair);
+    std::cout<<"Hand07: One-Voided Suit?"<<h07<<"\n";
+
+    bool h08=Hand08(vec1, vec2, vec3, vec4, pair);
+    std::cout<<"Hand08: No Honors?"<<h08<<"\n";
+
+    //Hand09 and 10 add point to player if they self draw or drew flowers in the game
+    //Skip 11-13
+    
+    bool h14=Hand14(vec1, vec2, vec3, vec4);
+    std::cout<<"Hand14: Dragon Pung?"<<h14<<"\n";
 
     return 0; 
 }
